df825a66568111e4d24599f3859aa31058a15626
Hugh Cole-Baker <sigmaris@gmail.com>
2025-02-02 20:20:58 +0000

    dockerfile is no longer needed for cross

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-ARG TARGET
-
-FROM rustembedded/cross:$TARGET-0.2.1
-
-ARG DEB_ARCH
-
-RUN dpkg --add-architecture $DEB_ARCH && \
-    apt-get update && \
-    apt-get install --assume-yes libudev1:$DEB_ARCH libudev-dev:$DEB_ARCH
--- build_images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-docker build -t sigmaris/avcontrolbuilder:armv7-unknown-linux-gnueabihf --build-arg TARGET=armv7-unknown-linux-gnueabihf --build-arg DEB_ARCH=armhf .
-docker build -t sigmaris/avcontrolbuilder:aarch64-unknown-linux-gnu --build-arg TARGET=aarch64-unknown-linux-gnu --build-arg DEB_ARCH=arm64 .
-
